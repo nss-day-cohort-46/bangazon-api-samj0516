d68df8ddbe767820142acf40802cfb650d4bda12
samj0516 <samj0516@gmail.com>
2021-06-04 14:48:04 -0500

    finished issue 13 user can post request to add user to favorite sellers list

--- bangazonapi/views/profile.py
+++ bangazonapi/views/profile.py
@@ -255,8 +255,8 @@ class Profile(ViewSet):
 
         return Response({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    @action(methods=['get'], detail=False)
-    def favoritesellers(self, request):
+    @action(methods=['get', 'post'], detail=False)
+    def favoritesellers(self, request, pk=None):
         """
         @api {GET} /profile/favoritesellers GET favorite sellers
         @apiName GetFavoriteSellers
@@ -305,11 +305,22 @@ class Profile(ViewSet):
         """
         customer = Customer.objects.get(user=request.auth.user)
         favorites = Favorite.objects.filter(customer=customer)
+        
+        if request.method == 'GET':
+            serializer = FavoriteSerializer(
+                favorites, many=True, context={'request': request})
+            return Response(serializer.data)
 
-        serializer = FavoriteSerializer(
-            favorites, many=True, context={'request': request})
-        return Response(serializer.data)
+        if request.method == 'POST':
+            try:
+                favorite_seller = Favorite()
+                favorite_seller.customer = customer
+                favorite_seller.seller = Customer.objects.get(user_id = request.data['seller'])
+                favorite_seller.save()
 
+                return Response(None, status=status.HTTP_204_NO_CONTENT)
+            except Customer.DoesNotExist as ex:
+                return Response(ex.args[0], status=status.HTTP_404_NOT_FOUND)
 
 class LineItemSerializer(serializers.HyperlinkedModelSerializer):
     """JSON serializer for products
